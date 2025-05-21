ef2c0db7755d942daa1d059f1d9370c519fe3fe2
htrgouvea <hi@heitorgouvea.me>
2025-05-21 20:19:35 -0300

    replace die for croak

--- lib/FuzzPM/Component/Case.pm
+++ lib/FuzzPM/Component/Case.pm
@@ -2,10 +2,11 @@ package FuzzPM::Component::Case {
     use strict;
     use warnings;
     use YAML::Tiny;
+    use Carp qw(croak);
 
     sub new {
         my ($file) = @_;
-        my $yaml = YAML::Tiny -> read($file) or die "Error reading file $file: $!";
+        my $yaml = YAML::Tiny -> read($file) or croak "Error reading file $file: $!";
 
         return $yaml -> [0] -> {test};
     }
