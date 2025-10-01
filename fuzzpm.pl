c59c3e2224eb48861f2f77f9ee4cf91a7e24d922
htrgouvea <hi@heitorgouvea.me>
2025-10-01 09:26:09 -0300

    better helper instructions

--- fuzzpm.pl
+++ fuzzpm.pl
@@ -1,7 +1,5 @@
 #!/usr/bin/env perl
 
-our $VERSION = '0.0.3';
-
 use strict;
 use warnings;
 use lib 'lib';
@@ -10,10 +8,20 @@ use FuzzPM::Component::Case;
 use FuzzPM::Network::Runner;
 use English qw(-no_match_vars);
 
+our $VERSION = '0.0.3';
+
 my $opts = FuzzPM::Component::CLI -> new();
 
 if ($opts -> {help} || !$opts -> {case}) {
-    print 'Usage: ' . $PROGRAM_NAME . " --case <file.yml> [--threads <num>]\n";
+    print "
+        \rFuzzPM v0.0.3
+        \rCore Commands
+        \r==============
+        \r\tCommand        Description
+        \r\t--------       -------------
+        \r\t--case         Define
+        \r\t--threads      Concorrent\n";
+    
 
     exit 0;
 }
