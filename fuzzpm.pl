dad275b1b571262d4261c87d3b28391d637ae9dd
htrgouvea <hi@heitorgouvea.me>
2025-10-01 09:27:47 -0300

    better helper instructions

--- fuzzpm.pl
+++ fuzzpm.pl
@@ -8,19 +8,19 @@ use FuzzPM::Component::Case;
 use FuzzPM::Network::Runner;
 use English qw(-no_match_vars);
 
-our $VERSION = '0.0.3';
+our $VERSION = '0.0.4';
 
 my $opts = FuzzPM::Component::CLI -> new();
 
 if ($opts -> {help} || !$opts -> {case}) {
     print "
-        \rFuzzPM v0.0.3
+        \rFuzzPM v$VERSION
         \rCore Commands
         \r==============
         \r\tCommand        Description
         \r\t--------       -------------
-        \r\t--case         Define
-        \r\t--threads      Concorrent\n";
+        \r\t--case         Define the use case of targets
+        \r\t--threads      Set the number of concurrent threads\n\n";
     
 
     exit 0;
