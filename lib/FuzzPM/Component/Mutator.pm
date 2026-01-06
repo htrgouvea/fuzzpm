413889cf793be5af8d252e73cfa51c51e969f577
htrgouvea <hi@heitorgouvea.me>
2026-01-06 13:54:39 -0300

    bit flip, byte overwrite/insert/delete, splice, token insert

--- lib/FuzzPM/Component/Mutator.pm
+++ lib/FuzzPM/Component/Mutator.pm
@@ -1,6 +1,7 @@
 package FuzzPM::Component::Mutator {
     use strict;
     use warnings;
+    use bytes;
     use Getopt::Long;
 
     our $VERSION = '0.0.4';
@@ -12,21 +13,111 @@ package FuzzPM::Component::Mutator {
             's|seed=s' => \$seed
         );
 
-        if (!$seed || length($seed) == 0) {
+        if (!defined $seed || length($seed) == 0) {
             return 0;
         }
 
-        my @chars = split qr//sm, $seed;
+        my $len = length $seed;
+        my @ops = qw(bit_flip byte_overwrite byte_insert token_insert splice byte_delete);
 
-        foreach my $i (0 .. $#chars) {
-            my $random = int rand @chars;
-            my $temp   = $chars[$i];
-
-            $chars[$i]      = $chars[$random];
-            $chars[$random] = $temp;
+        if ($len <= 1) {
+            @ops = grep { $_ ne 'byte_delete' && $_ ne 'splice' } @ops;
         }
 
-        return join q{}, @chars;
+        my $op = $ops[int rand @ops];
+
+        if ($op eq 'bit_flip') {
+            my @bytes = unpack 'C*', $seed;
+            my $index = int rand @bytes;
+            my $bit   = 1 << int rand 8;
+
+            $bytes[$index] ^= $bit;
+
+            return pack 'C*', @bytes;
+        }
+
+        if ($op eq 'byte_overwrite') {
+            my @bytes = unpack 'C*', $seed;
+            my $index = int rand @bytes;
+
+            $bytes[$index] = int rand 256;
+
+            return pack 'C*', @bytes;
+        }
+
+        if ($op eq 'byte_insert') {
+            my @bytes = unpack 'C*', $seed;
+            my $index = int rand(@bytes + 1);
+            my $byte  = int rand 256;
+
+            splice @bytes, $index, 0, $byte;
+
+            return pack 'C*', @bytes;
+        }
+
+        if ($op eq 'byte_delete') {
+            my @bytes = unpack 'C*', $seed;
+
+            return $seed if @bytes <= 1;
+
+            my $index = int rand @bytes;
+            splice @bytes, $index, 1;
+
+            return pack 'C*', @bytes;
+        }
+
+        if ($op eq 'splice') {
+            my @bytes = unpack 'C*', $seed;
+            my $count = @bytes;
+
+            return $seed if $count <= 1;
+
+            my $max_chunk  = $count < 8 ? $count : 8;
+            my $chunk_len  = 1 + int rand $max_chunk;
+            my $start      = int rand($count - $chunk_len + 1);
+            my @chunk      = splice @bytes, $start, $chunk_len;
+            my $insert_pos = int rand(@bytes + 1);
+
+            splice @bytes, $insert_pos, 0, @chunk;
+
+            return pack 'C*', @bytes;
+        }
+
+        if ($op eq 'token_insert') {
+            my @tokens = (
+                "\x00",
+                "\n",
+                "\r\n",
+                'A',
+                'AAAA',
+                '0',
+                '1',
+                '../',
+                '..\\',
+                '%s',
+                '%n',
+                q{"},
+                q{'},
+                '<',
+                '>',
+                '(',
+                ')',
+                '{',
+                '}',
+                '[',
+                ']',
+                q{\\},
+                '/',
+                ' ',
+            );
+
+            my $token = $tokens[int rand @tokens];
+            my $pos   = int rand(length($seed) + 1);
+
+            return substr($seed, 0, $pos) . $token . substr($seed, $pos);
+        }
+
+        return $seed;
     }
 }
 
